b89d0ed494bbdcefcce4440b90f46ac90142bfd6
Pierre Chambart <pierre.chambart@crans.org>
2015-03-15 01:32:48 +0100

    [info] Update krobot_rolling

--- example/krobot_rolling.ml
+++ example/krobot_rolling.ml
@@ -40,7 +40,7 @@ let move_x distance speed acceleration =
 *)
 
 let move_y distance speed acceleration =
-  send (Motor_move_x(distance,speed,acceleration))
+  send (Motor_move_y(distance,speed,acceleration))
 (** [Motor_move_y(distance, speed, acceleration)] command to make
     the robot to move.
     - [distance] is in m
@@ -64,6 +64,9 @@ let motor_stop lin_acc rot_acc = send (Motor_stop(lin_acc, rot_acc))
           - [rot_acc] in rad/s^2
       *)
 
+let reset_odometry () =
+  send (Krobot_message.Set_odometry(0.,0.,0.))
+
 let wait t = Lwt_unix.run (lwt () = Lwt_unix.sleep 0. in Lwt_unix.sleep t)
 (* en secondes *)
 
@@ -72,54 +75,33 @@ let wait_ref_false r =
     wait 0.01;
   done
 
-let beacon_speed s = send (Motor_command (2,s))
-
-
 let pi = 4. *. atan 1.
 
-(* let fait_un_truc () = *)
-(*   wait 2.; *)
-(*   Printf.printf "go\n%!"; *)
-(*   move 0.5 0.2 0.5; *)
-(*   wait_ref_false moving; *)
-(*   turn pi (pi/.2.) (pi/.2.); *)
-(*   wait_ref_false roting; *)
-(*   move 0.2 0.2 0.5 *)
+let () = move_x (-.0.05) 0.2 0.5
 
-let () = move_x 1. 0.2 0.5
+let () = move_x (-.0.5) 0.2 0.5
+let () = move_y 0.5 0.2 0.5
 
-let () = turn (pi/.2.) (pi/.2. /. 4.) (pi/.2. /. 4. /. 1.)
-let () = send (Set_odometry( 0.1, 0.2, pi))
+let () =
+  move_y (-.0.5) 0.2 0.5;
+  move_x 0.5 0.2 0.5
 
-let () = send (Drive_activation false)
-let () = send (Drive_activation true)
-let () = send (Drive_torque_limit 100)
-let () = send (Drive_torque_limit 3600)
-let () = send (Torque_limit(4,5))
-let () = send (Torque_limit(8,5))
+let () = reset_odometry ()
 
-let () = move 1. 0.2 0.5
-
-let () = move (-.1.) 0.2 0.5
-
-let () = turn (20.*.pi) pi 0.5
-
-let () = turn (-. 20.*.pi) pi 0.5
-
+let () = turn (-.2.*.pi) (pi/.2. /. 2.) (pi/.2. /. 4. /. 1.)
 let () = motor_stop 0.5 0.5
 
-(*lancer:
-  let () = fait_un_truc ()
-*)
 
+(*
 (* commande de la vm / planner *)
 open Krobot_geom
 
 let send_bus m = Lwt_unix.run (Krobot_bus.send bus (Unix.gettimeofday (), m))
 (* let stop () = send_bus Strategy_stop *)
 (* let clear () = send_bus (Trajectory_set_vertices []) *)
+
 (* nettoie la trajectoire enregistree *)
 let set_vertice x y = send_bus (Trajectory_set_vertices [{x;y}])
 (* enregistre la trajectoire *)
 let go () = send_bus Trajectory_go
-
+*)
