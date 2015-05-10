d3a4a825fd07bf5b4bbb48637563680fc3ff0f84
Pierre Chambart <pierre.chambart@crans.org>
2015-05-10 02:24:22 +0200

    [info] A few more stuff for arduino

--- src/tools/arduino/arduino.ml
+++ src/tools/arduino/arduino.ml
@@ -44,7 +44,7 @@ let handle_message info (timestamp, message) =
 
 let fork = ref true
 let tty = ref "/dev/ttyUSB1"
-let baudrate = ref 57600
+let baudrate = ref 115200
 
 let options = Arg.align [
   "-no-fork", Arg.Clear fork, " Run in foreground";
@@ -60,10 +60,30 @@ options are:"
    | Entry point                                                     |
    +-----------------------------------------------------------------+ *)
 
+type ('a, 'b) result =
+  | Ok of 'a
+  | Error of 'b
+
+let parse line =
+  try
+    Scanf.sscanf line "R%c%i" (fun c i -> Ok (c, i))
+  with Scanf.Scan_failure s -> Error s
+
+let count = ref 0
+
 let rec read_loop info serial =
-  (* ECHO *)
+  incr count;
+  let msg = Printf.sprintf "R%c%i" 'a' !count in
+  lwt () = Krobot_serial.write_line serial msg in
+  Printf.printf "send: %s\n%!" msg;
   lwt line = Krobot_serial.read_line serial in
-  lwt () = Krobot_serial.write_line serial line in
+  Printf.printf "received: %s\n%!" line;
+  begin match parse line with
+    | Error s -> Printf.printf "parse error: %s\n%!" s;
+    | Ok (c, i) ->
+      Printf.printf "parsed: %c %i\n%!" c i
+  end;
+  lwt () = Lwt_unix.sleep 0.1 in
   read_loop info serial
 
 lwt () =
