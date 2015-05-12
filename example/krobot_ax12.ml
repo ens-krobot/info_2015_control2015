9db10a34eebc01c388e9972b35ad4ebd07f7475e
Xavier Lagorce <Xavier.Lagorce@crans.org>
2015-05-12 04:12:12 +0200

    [info] More AX12 examples

--- example/krobot_ax12.ml
+++ example/krobot_ax12.ml
@@ -8,7 +8,7 @@ open Krobot_config;;
 
 (* le hub et driver doivent être lancés *)
 let bus = Lwt_unix.run (Krobot_bus.get ())
-let send m = Lwt_unix.run (Krobot_message.send bus (Unix.gettimeofday (), m))
+let send m = Krobot_message.send bus (Unix.gettimeofday (), m)
 let sends m =
   let t = Lwt_list.iter_s (fun m -> Krobot_message.send bus (Unix.gettimeofday (), m)) m in
   Lwt_unix.run t
@@ -24,13 +24,29 @@ let move_ax12 id position speed =
   send (Ax12_Goto (id, position, speed))
 let set_torque_ax12 id b =
   send (Ax12_Set_Torque_Enable (id, b))
+let move_ax12_reg id position speed =
+  send (Ax12_Goto_Reg (id, position, speed))
+let ax12_action () =
+  send (Ax12_Action (254))
+let silence_ax12 id =
+  send (Ax12_Status_Return_Level (id, 1))
 
-(* let () = *)
-(*   move_ax12 3 380 300 *)
-(* let () = *)
-(*   move_ax12 3 600 500 *)
-(* let () = *)
-(*   set_torque_ax12 3 false *)
+let do_stuff_simple () =
+  let (left_in, _, _, left_out) = left_arm_positions in
+  let (right_in, _, _, right_out) = right_arm_positions in
+
+  lwt () = move_ax12 left_arm_idx left_out 500 in
+  lwt () = Lwt_unix.sleep 0.01 in
+  lwt () = move_ax12 right_arm_idx right_out 500 in
+  lwt () = Lwt_unix.sleep 1. in
+  lwt () = move_ax12_reg left_arm_idx left_in 500 in
+  lwt () = Lwt_unix.sleep 0.01 in
+  lwt () = move_ax12_reg right_arm_idx right_in 500 in
+  lwt () = Lwt_unix.sleep 0.01 in
+  lwt () = ax12_action () in
+  Lwt.return ()
+
+let () = Lwt_unix.run (do_stuff_simple ())
 
 let do_stuff () =
   let (left_in, _, _, left_out) = left_arm_positions in
