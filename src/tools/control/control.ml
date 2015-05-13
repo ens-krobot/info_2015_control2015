541c92098ad7ced41b8db922fd6e74c366344627
Pierre Chambart <pierre.chambart@crans.org>
2015-05-13 20:00:57 +0200

    [info] set orientation for homologation

--- src/tools/control/control.ml
+++ src/tools/control/control.ml
@@ -1,74 +1,3 @@
-(* open Krobot_bus *)
-(* open Krobot_message *)
-(* open Krobot_geom *)
-(* open Krobot_world_update *)
-
-(* type world = Krobot_world_update.world *)
-
-(* type state = Idle *)
-
-(* type message = *)
-(*   | CAN of Krobot_message.t *)
-(*   | Msg of Krobot_bus.message *)
-
-(* type output = { *)
-(*   timeout : float; (\* in seconds *\) *)
-(*   messages : message list; *)
-(*   state : state; *)
-(*   world : world; *)
-(* } *)
-
-(* type input = *)
-(*   | Message of Krobot_bus.message *)
-(*   | World_updated of world_update *)
-(*   | Timeout *)
-
-(* let init_state = Idle *)
-
-(* let update_world world msg = *)
-(*   match Krobot_world_update.update_world world msg with *)
-(*   | Some (w, u) -> w, World_updated u *)
-(*   | None -> world, Message msg *)
-
-(* let step (input:input) (world:world) (state:state) : output = *)
-(*   { timeout = 1.; *)
-(*     messages = []; *)
-(*     state = state; *)
-(*     world = world } *)
-
-(* let send_msg bus time = function *)
-(*   | CAN c -> Krobot_bus.send bus (time, CAN (Info, Krobot_message.encode c)) *)
-(*   | Msg m -> Krobot_bus.send bus (time, m) *)
-
-(* let main_loop bus iter = *)
-(*   let rec aux world state timeout = *)
-(*     lwt msg = Krobot_entry_point.next ~timeout:Krobot_date.(time_to_wait timeout) iter  in *)
-(*     let world, input = *)
-(*       match msg with *)
-(*       | Krobot_entry_point.Timeout -> *)
-(*         (\* Lwt_log.ign_info_f "timeout"; *\) *)
-(*         world, Timeout *)
-(*       | Krobot_entry_point.Message(t,m) -> *)
-(*         update_world world m *)
-(*     in *)
-(*     let output = step input world state in *)
-(*     let time = Unix.gettimeofday () in *)
-(*     lwt () = Lwt_list.iter_s (fun m -> send_msg bus time m) output.messages in *)
-(*     let timeout_date = Krobot_date.(add (now ()) output.timeout) in *)
-(*     aux output.world output.state timeout_date in *)
-(*   aux init_world init_state (Krobot_date.now ()) *)
-
-(* module S : Krobot_entry_point.S = struct *)
-(*   let name = "control" *)
-(*   let options = [] *)
-(*   let main bus ev = *)
-(*     let iter = Krobot_entry_point.iterator_from_react ev in *)
-(*     main_loop bus iter *)
-(* end *)
-
-(* module Run = Krobot_entry_point.Make(S) *)
-
-
 open Krobot_geom;;
 open Krobot_mover_control;;
 
@@ -148,6 +77,18 @@ let command_goto args =
       (Array.length args);
     exit 1
 
+let command_turn args =
+  match args with
+  | [|theta|] ->
+    let theta = float_of_string theta in
+    lwt state = make () in
+    lwt state = retry_turn ~state ~orientation:theta in
+    Lwt.return ()
+  | _ ->
+    Printf.printf "goto: wrong number of arguments: %i, expected 1\n%!"
+      (Array.length args);
+    exit 1
+
 (****** Test homologatoin ************)
 
 type team = Krobot_bus.team
@@ -181,12 +122,17 @@ let push_position : Krobot_bus.team -> _  = function
   | Yellow -> yellow_position_to_push
   | Green -> mirror yellow_position_to_push
 
-let down_orientation = -. pi /. 2.
+let yellow_push_orientation = pi /. 4.
+let flip angle = pi -. angle
+
+let push_orientation : Krobot_bus.team -> _  = function
+  | Yellow -> yellow_push_orientation
+  | Green -> flip yellow_push_orientation
 
 let do_homologation_run state team =
   lwt state = retry_move ~state ~destination:(out_of_start_zone team) ~ignore_fixed_obstacles:true in
   lwt state = retry_goto ~state ~destination:(first_position team) in
-  lwt state = retry_turn ~state ~orientation:down_orientation in
+  lwt state = retry_turn ~state ~orientation:(push_orientation team) in
   lwt state = retry_move ~state ~destination:(push_position team) ~ignore_fixed_obstacles:false in
   Lwt.return ()
 
@@ -235,6 +181,8 @@ lwt () =
   match Sys.argv.(1) with
   | "goto" ->
     command_goto rest
+  | "turn" ->
+    command_turn rest
   | "demo" ->
     demo_loop ()
   | "run_homologation" ->
